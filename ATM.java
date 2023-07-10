06d3aad91d6f0cc2df4a7eea95e5565c8d051759
Raguraman <124240325+raguraman-1909@users.noreply.github.com>
2023-07-10 10:22:06 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,16 @@
+package atmProject;
+import java.io.IOException;
+
+public class ATM {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
+
